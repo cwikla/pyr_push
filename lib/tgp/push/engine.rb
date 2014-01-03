c2b6145ae51950edd71d048347f161cecb37ec3d
John Cwikla <cwikla@fwix.com>
2014-01-03 09:17:25 -0800

    Set default times to 9am - 8pm for sound.

--- lib/tgp/push/engine.rb
+++ lib/tgp/push/engine.rb
@@ -3,8 +3,8 @@ module Tgp
     class Engine < ::Rails::Engine
       config.tgp_push_apns_arn = nil
       config.tgp_push_gcm_arn = nil
-      config.tgp_push_start_message_time = 0
-      config.tgp_push_end_message_time = 2400
+      config.tgp_push_start_message_time = 900
+      config.tgp_push_end_message_time = 2000
       config.tgp_push_ttl = 600 # 10 minutes
       config.tgp_push_sound = "default"
     end

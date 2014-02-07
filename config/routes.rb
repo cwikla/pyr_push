6f28193bec0c7d8a6b9d02c072b0f5609755c53b
cwikla <cwikla@thegiantpixel.com>
2014-02-06 19:41:59 -0800

    Add routes to hide device tokens.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   match '/push_notifications/:id(.:format)' => "tgp/push/push_notifications#create", :via => :post
   match '/push_notifications/:id(.:format)' => "tgp/push/push_notifications#destroy", :via => :delete
+  match '/push_notifications(.:format)' => "tgp/push/push_notifications#create", :via => :post
+  match '/push_notifications(.:format)' => "tgp/push/push_notifications#destroy", :via => :delete
 end

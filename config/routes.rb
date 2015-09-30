e5b9d5cde3caa4feeda1856bd8ebdc08c14006c0
Brett Trimble <brett@thegiantpixel.com>
2015-09-30 12:17:45 -0700

    Removing routes that would have errored.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do
   match '/push_notifications/:id(.:format)' => "tgp/push/push_notifications#create", :via => :post
   match '/push_notifications/:id(.:format)' => "tgp/push/push_notifications#destroy", :via => :delete
-  match '/push_notifications(.:format)' => "tgp/push/push_notifications#create", :via => :post
-  match '/push_notifications(.:format)' => "tgp/push/push_notifications#destroy", :via => :delete
 end
